6781ad82180b31ff2e9d3e79043e8bef10b51335
Veronica <sivmskm205@gmail.com>
2024-04-14 12:21:26 +0530

    Area of circle

---
+++ p45.py
@@ -0,0 +1,8 @@
+#find area of circle 
+
+def get_area_of_circle( radius:float):
+    return 3.14*radius**2 
+
+inp1 = int(input("Enter the radius of the circle\n"))
+
+print(get_area_of_circle(inp1))
