7d95fceddab2c00a7bdf8346938728fa3c452277
Veronica <sivmskm205@gmail.com>
2024-04-15 08:47:50 +0530

    Numbers into words upto 10000

--- p48.py
+++ p48.py
@@ -57,22 +57,32 @@ def conver_number_to_words(number:int):
             elif first_digit==0 and mid_digit>0 and third_digit>0:
                 print(arr[third_digit] + ' hundred and '+ arr_2[mid_digit-1])
                 return 1
-                
             
+
         
+    elif len(str(number)) == 4:
+        first_digit = number % 10
+        mid_digit = number // 10 % 10
+        third_digit = number // 100 % 10
+        fourth_digit = number // 1000
         
-        
-    elif len(str(number))==4:
-        first_digit = number%10
-        mid_digit = number//10 %10 
-        third_digit =  number//100 %10
-        fourth_digit = number//1000
-        
-        for i in range(1,11):
-            if mid_digit==0 and first_digit==0 and third_digit==0 :
-                print(arr[fourth_digit] + ' thousand')
-                return 1
+        if mid_digit == 0 and third_digit == 0 and first_digit == 0:
+            print(arr[fourth_digit] + ' ' + arr_4[1])
+        elif third_digit == 0 and first_digit == 0:
+            print(arr[fourth_digit] + ' ' + arr_4[1] + ' and ' + arr_2[mid_digit - 1] + ' ' + arr[first_digit])
+        elif mid_digit == 0 and first_digit == 0:
+            print(arr[fourth_digit] + ' ' + arr_4[1] + ' and ' + arr[third_digit] + ' ' + arr_4[0])
+        elif mid_digit == 0 and third_digit == 0:
+            print(arr[fourth_digit] + ' ' + arr_4[1] + ' and ' + arr[first_digit])
+        elif third_digit == 0:
+            print(arr[fourth_digit] + ' ' + arr_4[1] + ' and ' + arr_2[mid_digit - 1] + ' ' + arr[first_digit] + ' ' + arr_4[0])
+        elif mid_digit == 0:
+            print(arr[fourth_digit] + ' ' + arr_4[1] + ' and ' + arr[third_digit] + ' ' + arr_4[0] + ' and ' + arr[first_digit])
+        else:
+            print(arr[fourth_digit] + ' ' + arr_4[1] + ' and ' + arr[third_digit] + ' ' + arr_4[0] + ' and ' + arr_2[mid_digit - 1] + ' ' + arr[first_digit])
             
+
+
 
 
 
@@ -89,3 +99,12 @@ conver_number_to_words(121)
 conver_number_to_words(327)
 conver_number_to_words(107)
 conver_number_to_words(333)
+conver_number_to_words(1000)
+conver_number_to_words(1001)
+conver_number_to_words(1010)
+conver_number_to_words(1100)
+conver_number_to_words(1101)
+conver_number_to_words(1110)
+conver_number_to_words(1111)
+conver_number_to_words(1119)
+conver_number_to_words(1121)
